c6177c91004b666d7840cda33dcd63ef3e476c1b
wirthsarah <wirthsarah1@gmail.com>
2024-05-28 09:16:27 +0200

    preliminary results

---
+++ cma-week5-rexercise.R
@@ -0,0 +1,41 @@
+#Task 1 ----
+
+bmi <- \(height, weight) weight/(height^2)
+
+deg_far <-\(celsius) celsius*9/5+32
+
+euclidean_distance <-\(x1,x2,y1,y2)  sqrt((x2-x1)^2+(y2-y1)^2)
+
+#Task 2 ----
+library("readr")
+library("dplyr")
+library("sf")
+library("lubridate")
+library("ggplot2")
+
+wildschwein <- read_delim("wildschwein_BE_2056.csv", ",")
+
+wildschwein <- wildschwein |>
+  filter(TierName =="Sabi"|TierName=="Rosa", DatetimeUTC > "2015-04-01 02:00:00", DatetimeUTC < "2015-04-16 02:00:00")#weird 02:00:00 timestamp because of the timezone-format
+
+#Task 3 ----
+wildschwein$DatetimeUTC <-round_date(wildschwein$DatetimeUTC,"15 mins")
+
+#Task 4 ----
+sabi<-wildschwein |>filter(TierName =="Sabi")
+rosa<-wildschwein |>filter(TierName =="Rosa")
+
+beide <- inner_join(sabi,rosa,by=join_by(DatetimeUTC), suffix=c(".Sabi",".Rosa"))
+beide$euclidean <-euclidean_distance(beide$E.Sabi, beide$E.Rosa, beide$N.Sabi, beide$N.Rosa)
+
+beide <- beide |>
+  mutate(meet = euclidean <= 100)
+
+#Task 5 ----
+ggplot()+
+  geom_point(sabi, mapping=aes(x=E,y=N), color="#00BFC4", alpha=0.3)+
+  geom_point(rosa, mapping=aes(x=E,y=N),color="#F8766D", alpha=0.3)+
+  geom_point(filter(beide,meet==TRUE),mapping=aes(x=E.Sabi,y=N.Sabi),pch=21, alpha=0.7,fill="#00BFC4", color="black")+
+  geom_point(filter(beide,meet==TRUE),mapping=aes(x=E.Rosa,y=N.Rosa),pch=21, alpha=0.7,fill="#F8766D", color="black")
+
+#Task 6 ----
