8787bcdc33e8e59320a8ecdd1ff25ac573c97304
wirthsarah <wirthsarah1@gmail.com>
2024-05-28 16:28:59 +0200

    final commit

--- cma-week5-rexercise.R
+++ cma-week5-rexercise.R
@@ -12,6 +12,7 @@ library("dplyr")
 library("sf")
 library("lubridate")
 library("ggplot2")
+library("plotly")
 
 wildschwein <- read_delim("wildschwein_BE_2056.csv", ",")
 
@@ -32,10 +33,27 @@ beide <- beide |>
   mutate(meet = euclidean <= 100)
 
 #Task 5 ----
+beide_filter <- beide |>  filter(meet==TRUE)
+
 ggplot()+
   geom_point(sabi, mapping=aes(x=E,y=N), color="#00BFC4", alpha=0.3)+
   geom_point(rosa, mapping=aes(x=E,y=N),color="#F8766D", alpha=0.3)+
-  geom_point(filter(beide,meet==TRUE),mapping=aes(x=E.Sabi,y=N.Sabi),pch=21, alpha=0.7,fill="#00BFC4", color="black")+
-  geom_point(filter(beide,meet==TRUE),mapping=aes(x=E.Rosa,y=N.Rosa),pch=21, alpha=0.7,fill="#F8766D", color="black")
+  geom_point(beide_filter,mapping=aes(x=E.Sabi,y=N.Sabi),pch=21, alpha=0.7,fill="#00BFC4", color="black")+
+  geom_point(beide_filter,mapping=aes(x=E.Rosa,y=N.Rosa),pch=21, alpha=0.7,fill="#F8766D", color="black")
 
 #Task 6 ----
+#this part is adapted from Saskia's code
+plot_ly(beide, x = ~E.Rosa, y = ~N.Rosa, z = ~DatetimeUTC, 
+        type = 'scatter3d', mode = 'lines', opacity = 0.4, 
+        line = list(width = 6, color = "#F8766D", reverscale = FALSE)) |> 
+  add_trace(beide, x = ~E.Sabi, y = ~N.Sabi, z = ~DatetimeUTC, 
+            type = 'scatter3d', mode = 'lines', opacity = 0.4, 
+            line = list(width = 6, color = "#00BFC4", reverscale = FALSE)) |>
+  add_markers(beide_filter, x = ~beide_filter$E.Rosa, y = ~beide_filter$N.Rosa, 
+              z = ~beide_filter$DatetimeUTC, 
+              type = 'scatter3d', mode = 'markers', opacity = 0.8, 
+              marker = list(size = 6, color = "#F8766D"))  |> 
+  add_markers(beide_filter, x = ~beide_filter$E.Sabi, y = ~beide_filter$N.Sabi, 
+              z = ~beide_filter$DatetimeUTC, 
+              type = 'scatter3d', mode = 'markers', opacity = 0.8, 
+              marker = list(size = 6, color = "#00BFC4", reverscale = FALSE)) 
